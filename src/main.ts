e1ca1a90c276124f6d77e943ae422991c1307ab7
Valentin Ispas <valentin.ispas@nagarro.com>
2021-10-17 08:57:45 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { setupApp } from './setup-app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    })
-  )
+  setupApp(app);
   await app.listen(3000);
 }
 bootstrap();
